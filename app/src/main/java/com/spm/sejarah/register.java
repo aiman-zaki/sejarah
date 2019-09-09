f42fbf3406962db2e9befff5900c660f11337774
syafiranasirr <55028837+syafiranasirr@users.noreply.github.com>
2019-09-10 01:23:49 +0800

    Interface Register

--- app/src/main/java/com/spm/sejarah/register.java
+++ app/src/main/java/com/spm/sejarah/register.java
@@ -2,13 +2,25 @@ package com.spm.sejarah;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class register extends AppCompatActivity {
+public abstract class register extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Spinner spinner = findViewById(R.id.spinner1);
+        Context context;
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.role, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }

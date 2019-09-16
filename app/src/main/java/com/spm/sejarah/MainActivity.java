865fa3e2a9be1e38ffa781ef1d46438d1742d90a
syafiranasirr <55028837+syafiranasirr@users.noreply.github.com>
2019-09-16 23:48:49 +0800

    Add login function (1 error)

--- app/src/main/java/com/spm/sejarah/MainActivity.java
+++ app/src/main/java/com/spm/sejarah/MainActivity.java
@@ -1,30 +1,98 @@
 package com.spm.sejarah;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.widget.Button;
 import android.view.View;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-import static com.spm.sejarah.R.id.btnlogin;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import com.spm.sejarah.ui.login.LoginActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button a;
+    private EditText inputic,inputpw;
+    private Button btnLogin;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        a= (Button) findViewById(btnlogin);
-        a.setOnClickListener(new View.OnClickListener() {
+
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(MainActivity.this, menupage.class));
+            finish();
+        }
+
+        // set the view now
+        setContentView(R.layout.activity_main);
+
+        inputic = (EditText) findViewById(R.id.username);
+        inputpw = (EditText) findViewById(R.id.password);
+        btnLogin = (Button) findViewById(R.id.btnlogin);
+
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this,menupage.class);
-                startActivity(i); }
+                String email = inputic.getText().toString();
+                final String password = inputpw.getText().toString();
+
+                if (TextUtils.isEmpty(username)) {
+                    Toast.makeText(getApplicationContext(), "Enter identification number!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                //authenticate user
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+
+                                if (!task.isSuccessful()) {
+                                    // there was an error
+                                    if (password.length() < 6) {
+                                        inputpw.setError(getString(R.string.invalid_password));
+                                    } else {
+                                        Toast.makeText(MainActivity.this, getString(R.string.login_failed), Toast.LENGTH_LONG).show();
+                                    }
+                                } else {
+                                    Intent intent = new Intent(MainActivity.this,menupage.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
+            }
         });
 
 

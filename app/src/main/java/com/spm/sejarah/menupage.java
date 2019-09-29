6a9f6f47e02f6d646cd87bd77155648692404e5f
syafiranasirr <55028837+syafiranasirr@users.noreply.github.com>
2019-09-29 17:28:05 +0800

    add viewflipper

--- app/src/main/java/com/spm/sejarah/menupage.java
+++ app/src/main/java/com/spm/sejarah/menupage.java
@@ -1,14 +1,31 @@
 package com.spm.sejarah;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class menupage extends AppCompatActivity {
+    private TextView f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menupage);
+
+        f= (TextView) findViewById(R.id.btnnota);
+        f.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(menupage.this,notaform4.class);
+                startActivity(i); }
+        });
+
+
+
+
     }
+
 }

da149d13464430d7e3d5afad2f66a0b157b5a62a
syafiranasirr <55028837+syafiranasirr@users.noreply.github.com>
2019-11-08 11:58:50 +0800

    preview pdf file (error) 2.0

---
+++ app/src/main/java/com/spm/sejarah/listKelas.java
@@ -0,0 +1,35 @@
+package com.spm.sejarah;
+
+public class listKelas {
+
+    public String kelasId;
+    public String teachIc;
+    public String kelasName;
+
+    public listKelas() {
+    }
+
+    public String getKelasId() {
+        return kelasId;
+    }
+
+    public void setKelasId(String kelasId) {
+        this.kelasId = kelasId;
+    }
+
+    public String getTeachIc() {
+        return teachIc;
+    }
+
+    public void setTeachIc(String teachIc) {
+        this.teachIc = teachIc;
+    }
+
+    public String getKelasName() {
+        return kelasName;
+    }
+
+    public void setKelasName(String kelasName) {
+        this.kelasName = kelasName;
+    }
+}
